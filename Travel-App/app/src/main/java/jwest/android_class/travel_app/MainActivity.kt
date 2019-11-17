12eb24af59655d3fbe74be043f20529a743ab39b
jwest115 <justinewest14@gmail.com>
2019-11-17 01:37:59 -0800

    added fragments

--- Travel-App/app/src/main/java/jwest/android_class/travel_app/MainActivity.kt
+++ Travel-App/app/src/main/java/jwest/android_class/travel_app/MainActivity.kt
@@ -1,28 +1,31 @@
-package com.example.android.navigation
+package jwest.android_class.travel_app
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
-import androidx.drawerlayout.widget.DrawerLayout
-import j.edu.travel_app.R
-import j.edu.travel_app.databinding.ActivityMainBinding
+import jwest.android_class.travel_app.databinding.ActivityMainBinding
 
-        class MainActivity : AppCompatActivity() {
-            private lateinit var drawerLayout: DrawerLayout
-            override fun onCreate(savedInstanceState: Bundle?) {
-                super.onCreate(savedInstanceState)
-                @Suppress("UNUSED_VARIABLE")
-                val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-                drawerLayout = binding.drawerLayout
+class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        @Suppress("UNUSED_VARIABLE")
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
         //getting a navController object
         val navController = this.findNavController(R.id.myNavHostFragment)
 
         //links navigation controller to the app bar
-        NavigationUI.setupActionBarWithNavController(this,navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
         NavigationUI.setupWithNavController(binding.navView, navController)
         NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        Log.d("here", " main activity")
     }
 
 
@@ -33,3 +36,4 @@ import j.edu.travel_app.databinding.ActivityMainBinding
         return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
+
--- TravelApp/app/src/main/java/j/edu/travel_app/MapFragment.kt
+++ TravelApp/app/src/main/java/j/edu/travel_app/MapFragment.kt
@@ -6,19 +6,63 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.google.android.gms.maps.GoogleMap
+import j.edu.travel_app.databinding.FragmentMapBinding
+import j.edu.travel_app.databinding.FragmentTitleBinding
+import androidx.appcompat.app.AppCompatActivity
 
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 /**
  * A simple [Fragment] subclass.
  */
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
 
-    override fun onCreateView(
+    private lateinit var mMap: GoogleMap
+    private lateinit var gGroup: Group
+    private lateinit var binding: FragmentMapBinding
+    var mapFragment : SupportMapFragment?=null
+
+    override fun onCreateView (
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        binding = DataBindingUtil.inflate<FragmentMapBinding>(
+            inflater,
+            R.layout.fragment_map, container, false
+        )
+        binding.mapFragment = this
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mapFragment = fragmentManager?.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+
+        // INITIALIZE THE GROUP
+        gGroup = Group()
+
+        // HARD CODE JUST FOR TESTING PURPOSES
+        gGroup.addMember(Member("Jose", "Garcia", 21, Coordinates(36.1699, -115.1398)))
+        gGroup.addMember(Member("Nairbis", "Sibrian", 21, Coordinates(38.7567, -120.8607)))
+        gGroup.addMember(Member("Ruben", "Garcia", 22, Coordinates(21.1619, -86.8515)))
+        gGroup.addMember(Member("Leslie", "Garcia", 21, Coordinates(19.8968, -155.5828)))
+        // Set the onClickListener for the submitButton
+        return binding.root
     }
-
-
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        gGroup.dispalyMembers(mMap)
+    }
 }
