dd7e384c29c25fc381729fff5ac8678208e1ab2c
jwest115 <justinewest14@gmail.com>
2019-11-17 01:40:54 -0800

    removing old project

--- TravelApp/app/src/main/java/j/edu/travel_app/Coordinates.kt
+++
@@ -1,9 +0,0 @@
-package j.edu.travel_app
-
-import com.google.android.gms.maps.model.LatLng
-
-class Coordinates(Lat : Double, Lng : Double) {
-    val latLng =  LatLng(Lat, Lng);
-    val Lat = Lat;
-    val Lng = Lng;
-}
