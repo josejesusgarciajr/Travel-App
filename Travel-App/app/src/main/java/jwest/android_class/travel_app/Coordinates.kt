2c910ec8aecce0ff71861fcf2cd91230ce4bbc2c
jwest115 <justinewest14@gmail.com>
2019-11-22 16:38:07 -0800

    deleted non-working version

--- Travel-App/app/src/main/java/jwest/android_class/travel_app/Coordinates.kt
+++
@@ -1,9 +0,0 @@
-package jwest.android_class.travel_app
-
-import com.google.android.gms.maps.model.LatLng
-
-class Coordinates(Lat : Double, Lng : Double) {
-    val latLng =  LatLng(Lat, Lng);
-    val Lat = Lat;
-    val Lng = Lng;
-}
