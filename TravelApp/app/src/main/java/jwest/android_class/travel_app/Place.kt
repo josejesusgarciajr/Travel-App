f57f88b790a5e3515c8cf26b6530493a719fd505
nikita1369 <nikitarely@gmail.com>
2019-12-07 22:05:15 -0800

    User will now have access to rate favorable landmarks that already exist

--- TravelApp/app/src/main/java/jwest/android_class/travel_app/Place.kt
+++
@@ -1,9 +0,0 @@
-package jwest.android_class.travel_app
-
-
-class Place(name: String, coordinates: Coordinates, rating: Int, description : String) {
-    var name = name;
-    var coordinates = coordinates;
-    var rating = rating;
-    var description = description;
-}
