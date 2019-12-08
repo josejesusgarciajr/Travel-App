369e4109e1ac3424228ce0f0580d8a09fa8f34c7
ArevaloBrandon115 <arevalobrandon115@gmail.com>
2019-12-08 14:20:52 -0800

    pop up for adding a landmark

--- TravelApp/app/src/main/java/jwest/android_class/travel_app/AddLandmarkFragment.kt
+++ TravelApp/app/src/main/java/jwest/android_class/travel_app/AddLandmarkFragment.kt
@@ -1,6 +1,7 @@
 package jwest.android_class.travel_app
 
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -13,6 +14,7 @@ import androidx.navigation.findNavController
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import jwest.android_class.travel_app.databinding.FragmentAddLandmarkBinding
+import kotlinx.android.synthetic.main.dialog_window.view.*
 import kotlin.math.roundToInt
 
 /**
@@ -32,8 +34,51 @@ class AddLandmarkFragment : Fragment() {
         // Inflate the layout for this fragment
         val args = AddLandmarkFragmentArgs.fromBundle(arguments!!)
 
-        binding.addLandmarkSubmitButton.setOnClickListener { createLandmark(args.latitude, args.longitude, binding.landmarkTitleInput.text.toString(), binding.landmarkDescriptionInput.text.toString(), binding.landmarkRatingInput.rating.toString().toFloat() ) }
+//        binding.addLandmarkSubmitButton.setOnClickListener { createLandmark(args.latitude, args.longitude, binding.landmarkTitleInput.text.toString(), binding.landmarkDescriptionInput.text.toString(), binding.landmarkRatingInput.rating.toString().toFloat() ) }
+        binding.addLandmarkSubmitButton.setOnClickListener{onClickShowRating(it)}
+
         return binding.root
+    }
+
+    fun onClickShowRating(view: View) {
+        binding.apply {
+            val builder = AlertDialog.Builder(view.context)
+
+            // Get layout inflater
+            val inflater = requireActivity().layoutInflater
+
+            // Inflate the dialog with custom window
+            val dialogView = inflater.inflate(R.layout.dialog_window, null)
+
+            // Inflate and set the layout for the dialog
+            // Pass null as the parent view because its going in the dialog layout
+            builder.setView(dialogView)
+                // Add action buttons
+                .setPositiveButton("OK")
+                { dialog, which ->
+
+
+                    var rating = dialogView.ratingBar.rating.toString()
+
+                    // User clicked OK button.
+                    Toast.makeText(
+                        view.context, "Pressed OK " + rating,
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    // Get the arguments passed into the fragment
+                    val args = AddLandmarkFragmentArgs.fromBundle(arguments!!)
+
+                    // Store landmark input in Firebase
+                    createLandmark(args.latitude, args.longitude, binding.landmarkTitleInput.text.toString(), binding.landmarkDescriptionInput.text.toString(), dialogView.ratingBar.rating )
+                }
+                .setNegativeButton("Cancel")
+                { dialog, which ->
+                    dialog.cancel()
+                }
+            builder.create()
+            builder.show()
+        }
     }
 
     private fun createLandmark(latitude : Float, longitude : Float, title : String, description : String, rating : Float){
