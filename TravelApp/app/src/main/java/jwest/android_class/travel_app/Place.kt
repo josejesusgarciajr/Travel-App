ed1b9ee2adab459fdbd2acd2f0a269917b001af2
nikita1369 <nikitarely@gmail.com>
2019-12-08 00:18:34 -0800

    Added missing files

---
+++ TravelApp/app/src/main/java/jwest/android_class/travel_app/Place.kt
@@ -0,0 +1,9 @@
+package jwest.android_class.travel_app
+
+
+class Place(name: String, coordinates: Coordinates, rating: Int, description : String) {
+    var name = name;
+    var coordinates = coordinates;
+    var rating = rating;
+    var description = description;
+}
